566cf9205fd01e115ed4353246eef2e87c06e874
Khushboo Priya <khushboopriyaiiitnr@gmail.com>
2021-02-17 00:54:17 +0530

    firstcommit

---
+++ epoch-to-human.js
@@ -0,0 +1,31 @@
+const year=document.querySelector('#year-output');
+const month=document.querySelector('#month-output');
+const date=document.querySelector('#date-output');
+const hour=document.querySelector('#hour-output');
+const minute=document.querySelector('#minute-output');
+const second=document.querySelector('#second-output');
+const button=document.querySelector('#convert-button');
+const input=document.querySelector('#epoch-input');
+const resetButton=document.querySelector('#reset-button');
+
+resetButton.addEventListener('click',()=>{
+    input.value = "";
+    year.value = "";
+    month.value = "";
+    date.value = "";
+    hour.value = "";
+    minute.value = "";
+    second.value = "";
+  });
+  
+  
+  
+  button.addEventListener('click',()=>{
+    let epochDate = new Date(parseInt(input.value));//1611041456000
+    year.value = epochDate.getFullYear();
+    month.value = epochDate.getMonth()+1;
+    date.value = epochDate.getDate();
+    hour.value = epochDate.getHours();
+    minute.value = epochDate.getMinutes();
+    second.value = epochDate.getSeconds(); 
+  });
